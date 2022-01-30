78b506f7d94f85f3e333d76847ee74c5fef5944d
Saúl García Céspedes <saul.garces09@gmail.com>
2022-01-30 23:37:03 +0100

    Update eslint support
    * Add google styleguide and refactor code
    * Added rules to match with Angular decorators
    * Set indent to 2 spaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
+/**
+ * Root Angular routing module
+ */
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
+/**
+ * Root Angular component
+ */
 export class AppComponent {
   title = 'portfolio';
 }
