28dd15057710161b46b884c4325e7b9ddd3cf3ad
Saúl García Céspedes <saul.garces09@gmail.com>
2022-04-01 21:22:11 +0200

    Navbar refactor to directives and environment

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
 import {Component} from '@angular/core';
+import {environment} from 'src/environments/environment';
 
+type link = {
+  tag: string;
+  url: string;
+}
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -9,4 +14,22 @@ import {Component} from '@angular/core';
  * NavbarComponent class
  */
 export class NavbarComponent {
+  /**
+   * Array of links
+   */
+  private _links: link[];
+
+  /**
+   * Links getter
+   */
+  public get links(): link[] {
+    return this._links;
+  }
+
+  /**
+   * NavbarComponent constructor
+   */
+  constructor() {
+    this._links = environment.NAVBAR_LINKS;
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,18 @@
 export const environment = {
   CURRENT_EMAIL: 'saul.garces09@gmail.com',
+  NAVBAR_LINKS: [
+    {
+      tag: 'About',
+      url: '#about',
+    },
+    {
+      tag: 'Experience',
+      url: '#experience',
+    },
+    {
+      tag: 'Contact',
+      url: '#contact',
+    },
+  ],
   production: true,
 };
