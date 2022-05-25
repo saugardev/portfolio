6f6947304a8de75205929418bed41fd011b72cd1
Saúl García Céspedes <saul.garces09@gmail.com>
2022-05-25 18:38:27 +0200

    Section movement by arrows

--- src/app/shared/components/arrow/arrow.component.ts
+++ src/app/shared/components/arrow/arrow.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {ViewportScroller} from '@angular/common';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-arrow',
@@ -9,4 +10,26 @@ import {Component} from '@angular/core';
  * ArrowComponent class
  */
 export class ArrowComponent {
+
+  /**
+   * Target section's id CSS Selector
+   */
+  @Input() sectionId: string;
+
+  /**
+   * ArrowComponent constructor
+   * @param {ViewportScroller} viewportScroller scroller instance
+   */
+  constructor(private viewportScroller: ViewportScroller) {
+    this.sectionId = '';
+  }
+
+  /**
+   * Scrolls to section if sectionId is 
+   */
+  public goToSection() {
+    this.sectionId === '' 
+      ? this.viewportScroller.scrollToPosition([0, 0])
+      : this.viewportScroller.scrollToAnchor(this.sectionId);
+  }
 }
