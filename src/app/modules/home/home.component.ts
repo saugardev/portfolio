a6b7cab1e01c7574a28b9acbab0dda367b498827
Saúl García Céspedes <saul.garces09@gmail.com>
2022-06-04 17:22:25 +0200

    Hello and about load animations

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {gsap} from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 @Component({
   selector: 'app-home',
@@ -8,5 +13,49 @@ import {Component} from '@angular/core';
 /**
  * HomeComponent class
  */
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  /**
+   * Hello div element
+   */
+  @ViewChild('hello', {static: true}) hello: ElementRef<HTMLDivElement>;
+  /**
+   * About section element
+   */
+  @ViewChild('about', {static: true}) about: ElementRef<HTMLDivElement>;
+
+  /**
+   * HomeComponent ngOnInit hook
+   */
+  ngOnInit(): void {
+    // Hello load
+    gsap.from(this.hello.nativeElement, {
+      duration: 0.4,
+      opacity: 0,
+      y: -20,
+      stagger: 0.2,
+      delay: 0.75,
+    });
+    // Hello animation
+    gsap.to(this.hello.nativeElement.childNodes, {
+      scrollTrigger: {
+        trigger: this.hello.nativeElement,
+        scrub: true,
+        start: '130% center',
+      },
+      color: '#fff',
+      scale: 0.8,
+    });
+    // About load
+    gsap.from(this.about.nativeElement.childNodes, {
+      scrollTrigger: {
+        trigger: this.about.nativeElement,
+        scrub: true,
+        end: '20% center',
+      },
+      duration: 0.4,
+      opacity: 0,
+      stagger: 0,
+      scale: 0.8,
+    });
+  }
 }
